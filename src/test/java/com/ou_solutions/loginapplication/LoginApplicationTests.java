f665729afd749a6605c1b4ff6e52ab607053ce5e
Omkar Upadhyay <omkarmh009@gmail.com>
2023-11-25 12:41:02 +0800

    Initial commit

---
+++ src/test/java/com/ou_solutions/loginapplication/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ou_solutions.loginapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
